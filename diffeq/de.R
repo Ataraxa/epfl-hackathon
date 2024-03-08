5e118d6a412854c0a8f840ae705a8e338283ba40
Ataraxa <alexandre_pere2033@yahoo.com>
2024-03-08 17:39:46 +0000

    added augur

--- diffeq/de.R
+++ diffeq/de.R
@@ -14,22 +14,25 @@ cat("Seurat Object Creation: done \n")
 seurat <- subset(seurat, subset = nFeature_RNA > 200 & nFeature_RNA < 6000)
 cat("Quality Control: done \n")
 
-## Standard Processing and Correction
+## Standard Processing
 seurat <- NormalizeData(seurat)
 seurat <- FindVariableFeatures(seurat, selection.method = "vst", nfeatures = 2000)
 seurat <- ScaleData(seurat)
+cat("Pre-processing: done! \n")
+
+## Dimensionality Reduction
 seurat <- RunPCA(seurat)
 seurat <- FindNeighbors(seurat, dims = 1:30, reduction = "pca")
 seurat <- FindClusters(seurat, resolution = 2, cluster.name = "unintegrated_clusters")
 seurat <- RunUMAP(seurat, dims = 1:30, reduction = "pca", reduction.name = "umap.unintegrated")
-cat("Pre-processing: done!")
+cat("Dimensionality Reduction: done! \n")
 
 ## Run differential expression analysis
 DE <- run_de(seurat,
     de_family=pseudobulk, de_method=DESeq2, de_type=LRT
     )
-cat("Differential Expression Analysis: done!")
 saveRDS(DE, "DE_results.rds")
+cat("Differential Expression Analysis: done! \n")
 
 # print(colnames(DE))
 # significant_rows <- DE[DE$p_val < 0.05, ]   
